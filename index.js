bbdcb05a7257fe0584c49ddb6ed11fec77c12e87
Barbara Stadnik <barbara1stadnik@gmail.com>
2019-12-07 22:29:59 +0100

    display friends in list

--- index.js
+++ index.js
@@ -2,31 +2,47 @@
 	'use strict';
 
 	if ('indexedDB' in window) {
-		var openRequest = window.indexedDB.open('exerciseDB', 1);
-
-		openRequest.onsuccess = function(event) {
-			var db = event.target.result;
-		}
-
-		openRequest.onerror = function(event) {
-			console.log('error', event.target.errorCode);
-		}
+		let openRequest = window.indexedDB.open('exerciseDB', 1);
 
 		openRequest.onupgradeneeded = function(event) {
-			var db = event.target.result,
+			let db = event.target.result,
 			objectStore = db.createObjectStore('friends');
 
 			objectStore.transaction.oncomplete = function(event) {
-				var transaction = db.transaction('friends', 'readwrite'),
+				let transaction = db.transaction('friends', 'readwrite'),
 				objectStore = transaction.objectStore('friends'),
 				friends = [{email: 'bob@bob.com', name: 'Bob'}, {email: 'jack@jack.com', name: 'Jack'},
 				{email: 'pete@pete.com', name: 'Pete'}, {email: 'jane@jane.com', name: 'Jane'}, 
 				{email: 'polly@polly.com', name: 'Polly'}];
 
-				for (var x in friends) {
+				for (let x in friends) {
 					objectStore.add(friends[x], x);
 				}
 			}
 		}
+
+		openRequest.onsuccess = function(event) {
+			let db = event.target.result,
+				transaction = db.transaction('friends', 'readonly'),
+				objectStore = transaction.objectStore('friends'),
+				getRequest = objectStore.getAll();
+
+			getRequest.onsuccess = function() {
+				let friends = getRequest.result,
+					friendsList = $('.friends');
+
+				displayFriendsList(friends, friendsList);
+			}
+		}
+
+		openRequest.onerror = function(event) {
+			console.log('error', event.target.errorCode);
+		}
+	}
+
+	function displayFriendsList(friends = [], list) {
+		friends.forEach((friend) => {
+			list.append(`<li>${friend.name} - ${friend.email}`);
+		});
 	}
 }()); 
